5333f9ccabf54b7a95ab700ca46349903dec2bdc
”John <102276922+UlianaNad@users.noreply.github.com>
2022-06-06 23:42:52 +0300

    06.06

--- js/main.js
+++ js/main.js
@@ -9,31 +9,22 @@ const run = () => {
     const valueDoor = elDoor.value;
     const valueSausage = elSausage.value;
 
-
-    if(valueDoor == 'open') {
-    elStatus.innerHTML = "The door is opened. You can take the sausage.";
-    return;
-
-    } else  if((valueDoor === 'open') && (valueSausage === 'take-out')) {
-        elStatus.innerHTML = "Take the sausage. Enjoy your meal!";
+    
+    if(valueDoor === "open" && valueSausage === "take-out") {
+        elStatus.innerHTML = 'You too the sausege.';
         return;
     }
+    if(valueDoor === "open" && valueSausage === "put-back") {
+        elStatus.innerHTML = 'Close the door please!';
+        return;
 
-    if((valueDoor == 'close') && (valueSausage == 'take-out') || (valueSausage == 'take-out') || (valueDoor == 'close')) {
+    }
+    if(valueDoor === "close"){
         elStatus.innerHTML = "The door is closed!";
         return;
     }
-
-    if((valueDoor == 'open') && (valueSausage === 'put-back')) {
-        elStatus.innerHTML = "You have put the sausage in the fridge. Close the door!";
-        return;
-    }
-    if(valueSausage === 'put-back') {
-        elStatus.innerHTML = "It is not possible! You did not take anything!";
-        return;
-    }
-    if ((valueDoor === 'choose1') || (valueSausage === 'choose2')) {
-        elStatus.innerHTML = "Choose option!";
+    if(valueDoor === "open" ) {
+        elStatus.innerHTML = 'You opened the door.';
         return;
     }
    
